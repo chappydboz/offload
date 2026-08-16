8593dc36723ef7644e0de665d68a7afdcae945d7
Dustin Bozarth <dustinbozarth@macbookpro.mynetworksettings.com>
2026-08-16 18:01:19 -0500

    Hide stock badge in cart cross-sells and fix recommended product image size

--- Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
+++ Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_DONK_TOSS_VERSION', '3.9.2' );
+define( 'CHILD_THEME_DONK_TOSS_VERSION', '3.9.3' );
 
 /**
  * Include Custom Post Type & ACF Events definitions
