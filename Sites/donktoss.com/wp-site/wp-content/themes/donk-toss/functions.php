318f9d47399d975ec0988def32ce9baa3d66039d
Dustin Bozarth <dustinbozarth@macbookpro.mynetworksettings.com>
2026-08-16 18:06:17 -0500

    Override .astra-shop-thumbnail-wrap width and bump theme version to 3.9.5

--- Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
+++ Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_DONK_TOSS_VERSION', '3.9.4' );
+define( 'CHILD_THEME_DONK_TOSS_VERSION', '3.9.5' );
 
 /**
  * Include Custom Post Type & ACF Events definitions
