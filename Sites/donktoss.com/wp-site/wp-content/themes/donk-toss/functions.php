84750159824d7a9d4634782fd595bfbb92d7ae59
Dustin Bozarth <dustin@purplebee.org>
2026-08-19 20:52:42 -0500

    fix(checkout): restore visibility on Save payment information checkbox label by overriding Astra modern-input opacity:0

--- Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
+++ Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.2.2' );
+define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.2.3' );
 
 /**
  * Include Custom Post Type & ACF Events definitions
