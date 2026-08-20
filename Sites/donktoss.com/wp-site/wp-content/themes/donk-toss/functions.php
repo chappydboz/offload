2ee3ebbea08e4c2dfd3145ed25c3322ff031a1f2
Dustin Bozarth <dustin@purplebee.org>
2026-08-19 20:50:03 -0500

    style(checkout): remove background and border from payment_box for clean seamless payment UI

--- Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
+++ Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.2.1' );
+define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.2.2' );
 
 /**
  * Include Custom Post Type & ACF Events definitions
