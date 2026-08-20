47565a5b985b80b24f69d842f4b6f33712f55046
Dustin Bozarth <dustin@purplebee.org>
2026-08-19 20:58:21 -0500

    style(checkout,cart): make 'Have a coupon?' text orange, 1.1rem, and bold across Cart and Checkout

--- Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
+++ Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.2.6' );
+define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.2.7' );
 
 /**
  * Include Custom Post Type & ACF Events definitions
