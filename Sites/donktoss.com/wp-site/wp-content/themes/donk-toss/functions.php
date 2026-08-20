03fdfd6917f89c2e9a4b09be22d25dd2ac3fa355
Dustin Bozarth <dustin@purplebee.org>
2026-08-19 20:01:14 -0500

    fix(checkout): override Astra Addon floating label specificity to force visible form field labels

--- Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
+++ Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.0.2' );
+define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.0.4' );
 
 /**
  * Include Custom Post Type & ACF Events definitions
