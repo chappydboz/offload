a98fe080ed4553069726752f315a1e5b6763d0f3
Dustin Bozarth <dustin@purplebee.org>
2026-08-22 14:42:00 -0500

    fix: remove all container overrides so site entirely uses Astra native container architecture

--- Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
+++ Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.5.4' );
+define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.5.5' );
 
 /**
  * Include Custom Post Type & ACF Events definitions
