55a06c3fa4156818ba19840e5a30f22776bb9e3a
Dustin Bozarth <dustin@purplebee.org>
2026-08-23 18:57:48 -0500

    Fix cart coupon layout & match checkout styling, fix empty notice, style email validation (v4.7.6)

--- Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
+++ Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.7.3' );
+define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.7.6' );
 
 /**
  * Include Custom Post Type & ACF Events definitions
