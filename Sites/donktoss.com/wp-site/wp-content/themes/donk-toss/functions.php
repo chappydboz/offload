7f785cbcd925f0eb40a3732ded2dfdae94ccdd30
Dustin Bozarth <dustin@purplebee.org>
2026-09-01 12:55:48 -0500

    fix(checkout): refine overview grid breakpoints and equal width columns (v4.8.9)

--- Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
+++ Sites/donktoss.com/wp-site/wp-content/themes/donk-toss/functions.php
@@ -11,7 +11,7 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.8.8' );
+define( 'CHILD_THEME_DONK_TOSS_VERSION', '4.8.9' );
 
 /**
  * Include Custom Post Type & ACF Events definitions
